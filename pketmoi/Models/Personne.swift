d9a72766d20e8db8a6f44d3f84e3e4cf7573faf5
Hugo Maitre <masterh@Hugos-Mac.local>
2018-02-20 17:23:33 +0100

    Commencement personne

---
+++ pketmoi/Models/Personne.swift
@@ -0,0 +1,26 @@
+//
+//  Personne.swift
+//  pketmoi
+//
+//  Created by Hugo Maitre on 20/02/2018.
+//  Copyright © 2018 Hugo Maitre. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Person type
+ 
+ Propriétés: 
+ 
+ **lastname** Person -> String
+ **firstname** Person -> String
+ **fullname** Person -> String
+ **age** Person -> Int?
+ **address** Person -> String
+ **
+*/
+
+class Person {
+    
+}
