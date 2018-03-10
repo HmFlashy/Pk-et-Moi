a21e670e6cda4dc69aa72a1ada908af8c9d3f9f6
Hugo Maitre <masterh@hugos-mac.local>
2018-03-10 01:54:17 +0100

    lol

--- pketmoi/Controllers/EventController.swift
+++ pketmoi/Controllers/EventController.swift
@@ -7,18 +7,39 @@
 //
 
 import UIKit
+import CoreData
 
-class EventController: UIViewController {
+class EventController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate, UITextViewDelegate {
     
     
     
     @IBOutlet weak var buttonTriggerEvent: UIButton!
     @IBOutlet weak var buttonAddEvent: UIButton!
     
+    @IBOutlet weak var eventTableView: EventTableView!
+    
+    let segueShowEvent = "showEvent"
+    
+    let eventPresenter = EventPresenter()
+    
+    fileprivate lazy var eventFetched: NSFetchedResultsController<Event> = {
+        let request: NSFetchRequest<Event> = Event.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key:#keyPath(Event.date), ascending: false)]
+        let fetchedResultController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: CoreDataManager.context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultController.delegate = self
+        return fetchedResultController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString)
-
+        eventTableView.dataSource = self
+        eventTableView.delegate = self
+        do {
+            try self.eventFetched.performFetch()
+        } catch {
+            print("Problem")
+        }
         buttonTriggerEvent.layer.cornerRadius = 8
         buttonAddEvent.layer.cornerRadius = 8
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,6 +48,80 @@ class EventController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - UITableViewDataSource required protocol
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let events = eventFetched.fetchedObjects else { return 0 }
+        return events.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = eventTableView.dequeueReusableCell(withIdentifier: "eventCell", for: indexPath)
+        eventPresenter.configureCell(forCell: cell, event: eventFetched.object(at: indexPath))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+        case .delete:
+            let event = eventFetched.object(at: indexPath)
+            CoreDataManager.context.delete(event)
+            do {
+                try CoreDataManager.context.save()
+            } catch let error as NSError {
+                print("Error saving context after delete: \(error.localizedDescription)")
+            }
+            break
+        default:break
+        }
+    }
+    
+    // MARK: - NSFetchResultController delegate protocol
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch (type) {
+        case .insert:
+            if let indexPath = newIndexPath {
+                self.eventTableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break
+        case .delete:
+            if let indexPath = indexPath {
+                self.eventTableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+        default:
+            break
+        }
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        self.eventTableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        self.eventTableView.endUpdates()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("mdr")
+        if segue.identifier == self.segueShowEvent{
+            if let indexPath = self.eventTableView.indexPathForSelectedRow{
+                let showEventViewController = segue.destination as! ShowEventViewController
+                showEventViewController.event = self.eventFetched.object(at: indexPath)
+                self.eventTableView.deselectRow(at: indexPath, animated: true)
+            }
+        }
+    }
+    
+    // MARK: - Action Handler
+    func deleteHandlerAction(action: UITableViewRowAction, indexPath: IndexPath) -> Void{
+        let event = self.eventFetched.object(at: indexPath)
+        CoreDataManager.context.delete(event)
     }
     
     
---
+++ pketmoi/Controllers/ShowEventViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ShowEventViewController.swift
+//  pketmoi
+//
+//  Created by Hugo Maitre on 09/03/2018.
+//  Copyright © 2018 Hugo Maitre. All rights reserved.
+//
+
+import CoreData
+import UIKit
+
+class ShowEventViewController: UIViewController {
+    
+    var event : Event? = nil
+    
+    @IBOutlet weak var dateEvent: UILabel!
+    @IBOutlet weak var typeEvent: UILabel!
+    @IBOutlet weak var descriptionEvent: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        // If an event has been set, then display it
+        if let aevent = self.event{
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "fr_FR")
+            dateFormatter.dateFormat = "dd/MM/YYYY hh:mm"
+            dateEvent.text = dateFormatter.string(from: aevent.date!)
+            typeEvent.text = aevent.belongsTo?.name
+            descriptionEvent.text = aevent.eventDescription
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
