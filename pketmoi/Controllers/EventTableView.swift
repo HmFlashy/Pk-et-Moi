f30c62b32232c93f436202260ef7fc033c2d4970
Hugo Maitre <masterh@hugos-mac.local>
2018-03-10 01:50:32 +0100

    Evenement presque fini

--- pketmoi/Controllers/EventTableView.swift
+++ pketmoi/Controllers/EventTableView.swift
@@ -9,92 +9,11 @@
 import UIKit
 import CoreData
 
-class EventTableView: UITableView, UITableViewDataSource, NSFetchedResultsControllerDelegate {
-    
-    fileprivate var eventPresenter: EventPresenter = EventPresenter()
-    
-    fileprivate lazy var eventFetched: NSFetchedResultsController<Event> = {
-        let request: NSFetchRequest<Event> = Event.fetchRequest()
-        request.sortDescriptors = [NSSortDescriptor(key:#keyPath(Event.date), ascending: true)]
-        let fetchedResultController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: CoreDataManager.context, sectionNameKeyPath: nil, cacheName: nil)
-        fetchedResultController.delegate = self
-        return fetchedResultController
-    }()
-    
+class EventTableView: UITableView {
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.dataSource = self
-        do {
-            try self.eventFetched.performFetch()
-        } catch {
-            print("Problem")
-        }
     }
-    
-    // MARK: - UITableViewDataSource required protocol
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let events = eventFetched.fetchedObjects else { return 0 }
-        return events.count
-    }
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = dequeueReusableCell(withIdentifier: "eventCell", for: indexPath)
-        eventPresenter.configureCell(forCell: cell, event: eventFetched.object(at: indexPath))
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        switch editingStyle {
-        case .delete:
-            let event = eventFetched.object(at: indexPath)
-            CoreDataManager.context.delete(event)
-            do {
-                try CoreDataManager.context.save()
-            } catch let error as NSError {
-                print("Error saving context after delete: \(error.localizedDescription)")
-            }
-            break
-        default:break
-        }
-    }
-    
-    // MARK: - NSFetchResultController delegate protocol
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        switch (type) {
-        case .insert:
-            if let indexPath = newIndexPath {
-                self.insertRows(at: [indexPath], with: .fade)
-            }
-            break
-        case .delete:
-            if let indexPath = indexPath {
-                self.deleteRows(at: [indexPath], with: .automatic)
-            }
-        default:
-            break
-        }
-    }
-    
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        self.beginUpdates()
-    }
-    
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        self.endUpdates()
-    }
-    
-    // MARK: - Action Handler
-    func deleteHandlerAction(action: UITableViewRowAction, indexPath: IndexPath) -> Void{
-        let event = self.eventFetched.object(at: indexPath)
-        CoreDataManager.context.delete(event)
-    }
-    
-    
     
     /*
      // Only override draw() if you perform custom drawing.
--- pketmoi/Controllers/TriggerEventViewController.swift
+++ pketmoi/Controllers/TriggerEventViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class TriggerEventViewController: UIViewController, NSFetchedResultsControllerDelegate {
+class TriggerEventViewController: UIViewController, NSFetchedResultsControllerDelegate, UITextViewDelegate {
     
     @IBOutlet weak var typeEventPickerView: TypeEventPickerViewController!
     
@@ -19,13 +19,12 @@ class TriggerEventViewController: UIViewController, NSFetchedResultsControllerDe
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        descriptionTextArea.delegate = self
     }
     
     @IBAction func addTriggerEvent(_ sender: Any) {
-        var event: Event = Event(context: CoreDataManager.context)
+        let event: Event = Event(context: CoreDataManager.context)
         event.belongsTo = typeEventPickerView.selectedTypeEvent
-        print(event.belongsTo?.name)
-        print(dataPicker.date)
         event.date = dataPicker.date
         event.eventDescription = descriptionTextArea.text
         do {
@@ -33,7 +32,33 @@ class TriggerEventViewController: UIViewController, NSFetchedResultsControllerDe
         } catch {
             print("Erreur")
         }
+    }
+    
+    
+    // MARKL: - TextViewDelegate
+    
+    // Start Editing The Text Field
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        moveTextView(textView, moveDistance: -250, up: true)
+    }
+    
+    // Finish Editing The Text Field
+    func textViewDidEndEditing(_ textView: UITextView) {
+        moveTextView(textView, moveDistance: -250, up: false)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        descriptionTextArea.resignFirstResponder()
+    }
+    
+    private func moveTextView(_ textView: UITextView, moveDistance: Int, up: Bool) {
+        let moveDuration = 0.3
+        let movement: CGFloat = CGFloat(up ? moveDistance : -moveDistance)
         
-        
+        UIView.beginAnimations("animateTextField", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(moveDuration)
+        self.view.frame = self.view.frame.offsetBy(dx: 0, dy: movement)
+        UIView.commitAnimations()
     }
 }
