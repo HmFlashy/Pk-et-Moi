1850a3ef3435867999c429f9c8b637550e8d1158
Hugo Maitre <masterh@hugos-mac.local>
2018-03-12 14:23:09 +0100

    animation

--- pketmoi/Controllers/TypeActivityController.swift
+++ pketmoi/Controllers/TypeActivityController.swift
@@ -19,16 +19,41 @@ class TypeActivityController: UIViewController{
         super.viewDidLoad()
         popUpView.layer.cornerRadius = 10
         popUpView.layer.masksToBounds = true
+        self.showAnimate()
     }
     
     @IBAction func addTypeActivity(_ sender: Any) {
         guard typeActivity.text != "" else {
-            return self.view.removeFromSuperview()
+            self.removeAnimate()
+            return
         }
         let typeActivityName: String = typeActivity.text!
         let descriptionTA: String = descriptionTypeActivity.text
         TypeActivity.createActivity(name: typeActivityName, description: descriptionTA)
         typeActivity.text = ""
-        self.view.removeFromSuperview()
+        self.removeAnimate()
+    }
+    
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    func removeAnimate()
+    {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.view.removeFromSuperview()
+            }
+        });
     }
 }
